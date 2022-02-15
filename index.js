2c7601bd5ebf30c5e1e8e2509fc0e3423e9b1572
21ciprian <2021ciprianoffice@gmail.com>
2022-02-15 22:42:56 +0000

    Added array of cities in js file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('linked')
+const cities = [
+	'LONDON',
+	'BIRMINGHAM',
+	'LEEDS',
+	'GLASGOW',
+	'EDINBURGH',
+	'LIVERPOOL',
+	'MANCHESTER',
+	'BRISTOL',
+	'CARDIFF',
+	'BELFAST',
+	'BRIGHTON',
+	'WESTMINSTER',
+	'YORK',
+	'CAMBRIDGE',
+	'OXFORD',
+]
